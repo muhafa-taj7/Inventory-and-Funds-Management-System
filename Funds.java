2daebe81625c1ff60bbe5d8b147754bc28de70be
muhafa-taj7 <152342679+muhafa-taj7@users.noreply.github.com>
2023-11-29 08:49:28 -0600

    Create Funds.java

---
+++ Funds.java
@@ -0,0 +1,16 @@
+//Rana Shui doThis();
+
+class Funds{
+    public appendToWishlistCost(int a){
+
+    }
+    
+    public void printList(){
+        System.out.println("-----Total Wishlist------");
+        for(InventoryObject e: InventoryObject.items){
+            System.out.println(e.printObjectWishlist());
+        }
+        System.out.println(------Total Cost----------\n"+wishlistCost);
+        
+    }
+}
