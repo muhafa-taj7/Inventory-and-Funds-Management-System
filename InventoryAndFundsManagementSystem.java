bec5cc23f8d6e5e97c5562e28ebb3fbeda1a88b5
muhafa-taj7 <152342679+muhafa-taj7@users.noreply.github.com>
2023-11-28 18:55:31 -0600

    Add files via upload

---
+++ InventoryAndFundsManagementSystem.java
@@ -0,0 +1,19 @@
+// MustafaTajammul, Abdurrahim Rana, Shoaib Hussain
+class Funds{
+    
+}
+
+class InventoryObject extends Funds{
+    private String name;
+    private int wishlistCount;
+    private double price;
+    private int purchasedCount;
+    private static int count;
+    ArrayList<InventoryObject> items = new ArrayList<InventoryObject>;
+    
+    
+}
+
+class countFRC extends InventoryObject{
+    
+}
