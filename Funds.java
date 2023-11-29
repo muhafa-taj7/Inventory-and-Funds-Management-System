fd8b46e92121871b6004eec7fd4e139eb71323a4
muhafa-taj7 <152342679+muhafa-taj7@users.noreply.github.com>
2023-11-29 08:54:37 -0600

    Update Funds.java

--- Funds.java
+++ Funds.java
@@ -8,7 +8,7 @@ class Funds{
     public void printList(){
         System.out.println("-----Total Wishlist------");
         for(InventoryObject e: InventoryObject.items){
-            System.out.println(e.printObjectWishlist());
+            System.out.println(printObjectWishlist(e));
         }
         System.out.println(------Total Cost----------\n"+wishlistCost);
         
